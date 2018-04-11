72214a9c63cd58b64872729516beb42c3824e9be
Ben Atkins <benjatkins13@gmail.com>
2018-04-11 10:49:36 -0500

    exercise 2

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import guides from './guide'
 // import prettyPicture from './images/pretty.png';
 
 
 function Body(props){
     return (
-        <div className="App-body">
+        <div id="App-body">
         <img src={props.pic} className="main-pic" alt="pretty" />
+        <h2>Available Guides</h2>
+        <div id="guide-list">
+        {props.guideList}
+        </div>
+
       </div>
     )
 
